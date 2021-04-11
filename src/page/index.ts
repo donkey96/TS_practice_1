a759d54125aaf04acfe8f174a86c1e231f4bc658
donkey96 <[noire964@gmail.com]>
2021-04-11 21:22:30 +0900

    Update index.ts

--- src/page/index.ts
+++ src/page/index.ts
@@ -1,7 +1,7 @@
 import { Bank } from "../model/bank";
 
-let bank_a = new Bank("三菱東京UFJ");
-let bank_b = new Bank("ゆうちょ銀行");
-let bank_c = new Bank("みずほ銀行");
+let bank_a = new Bank("三菱東京UFJ", "UFJカード", 300000);
+let bank_b = new Bank("ゆうちょ銀行", "ゆうちょカード", 100000);
+let bank_c = new Bank("みずほ銀行", "みずほカード", 1000000);
 
 console.log(bank_a, bank_b, bank_c);

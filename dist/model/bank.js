df5aa63e5f116ca6d2ec086798ede5a15164a935
donkey96 <[noire964@gmail.com]>
2021-04-11 21:21:57 +0900

    Create .gitignore

--- dist/model/bank.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Bank = void 0;
-class Bank {
-    constructor(name) {
-        this.name = name;
-    }
-}
-exports.Bank = Bank;
--- dist/page/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const bank_1 = require("../model/bank");
-let bank_a = new bank_1.Bank("三菱東京UFJ");
-let bank_b = new bank_1.Bank("ゆうちょ銀行");
-let bank_c = new bank_1.Bank("みずほ銀行");
-console.log(bank_a, bank_b, bank_c);
